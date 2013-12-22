d74d3b3f7d7a2e27a04d9125e86be65a3b4bdfb2
Ian Heggie <ian@heggie.biz>
2013-12-23 06:27:59 +1100

    Fix logger install;

--- lib/rawk_log.rb
+++ lib/rawk_log.rb
@@ -3,9 +3,10 @@ require "rawk_log/version"
 # Don't load automatically
 #require "rawk_log/command"
 
-if defined?(ActiveSupport::BufferedLogger)
+if defined?(Logger)
   require "rawk_log/patch_logger"
 end
+
 if defined?(ActiveSupport::BufferedLogger)
   require "rawk_log/patch_activesupport_bufferedlogger"
 end
