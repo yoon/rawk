3f463323d874c29f5ec5c09a7fd78000b12e8775
Ian Heggie <ian@heggie.biz>
2013-12-23 17:36:38 +1100

    All logger patching in one file rawk_log/patch_logger

--- lib/rawk_log.rb
+++ lib/rawk_log.rb
@@ -3,13 +3,6 @@ require "rawk_log/version"
 # Don't load automatically
 #require "rawk_log/command"
 
-if defined?(Logger)
-  require "rawk_log/patch_logger"
-end
-
-if defined?(ActiveSupport::BufferedLogger)
-  require "rawk_log/patch_activesupport_bufferedlogger"
-end
 
 module RawkLog
   # Everything loaded in requires above
--- lib/rawk_log/patch_logger.rb
+++ lib/rawk_log/patch_logger.rb
@@ -1,7 +1,34 @@
-
+if defined?(Logger) and Logger.respond_to?(:format_message)
 class Logger
   def format_message(severity, timestamp, progname, msg)
     "#{msg} (pid:#{$$})\n"
   end
 end
+module RawkLog
+  PATCHED_LOGGER = true
+end
+end
 
+if defined?(ActiveSupport::BufferedLogger) and ActiveSupport::BufferedLogger.respond_to?(:add)
+module ActiveSupport
+  # Format the buffered logger with timestamp/severity info.
+  class BufferedLogger
+    def add(severity, message = nil, progname = nil, &block)
+      return if @level > severity
+      message = (message || (block && block.call) || progname).to_s
+      # If a newline is necessary then create a new message ending with a newline.
+      # Ensures that the original message is not mutated.
+      message = "#{message}\n" unless message[-1] == ?\n
+      message = message.gsub(/\n/," (pid:#{$$})\n")
+      buffer << message
+      auto_flush
+      message
+    end
+  end
+end
+
+
+module RawkLog
+  PATCHED_BUFFERED_LOGGER = true
+end
+end
